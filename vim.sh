72c4b5110347def1a3deea8c366ea4400699defd
matoruru <matoruru@shgt.jp>
2018-06-20 12:37:52 +0000

    modify vim installer

--- vim.sh
+++ vim.sh
@@ -1,24 +1,22 @@
 echo '[Install languages (for vim):]'
-emerge dev-lang/ruby
-emerge dev-lang/lua
-emerge dev-lang/luajit
+sudo emerge --ask dev-lang/ruby dev-lang/lua dev-lang/luajit
 
 echo  '[Install vim:]'
 cd    ~/repositories
 git   clone https://github.com/vim/vim.git
 cd    vim/src
-./configure --enable-multibyte             \
-            --enable-luainterp=dynamic     \
-            --enable-perlinterp=dynamic    \
-            --enable-perlinterp=dynamic    \
-            --enable-pythoninterp=dynamic  \
-            --enable-python3interp=dynamic \
-            --enable-rubyinterp=dynamic    \
-            --enable-fontset               \
-            --enable-fail-if-missing       \
-            --with-luajit                  \
-            --with-features=huge           \
-            && make && make install
+
+sudo ./configure --enable-multibyte             \
+                 --enable-luainterp=dynamic     \
+                 --enable-perlinterp=dynamic    \
+                 --enable-pythoninterp=dynamic  \
+                 --enable-python3interp=dynamic \
+                 --enable-rubyinterp=dynamic    \
+                 --enable-fontset               \
+                 --enable-fail-if-missing       \
+                 --with-luajit                  \
+                 --with-features=huge           \
+                 && sudo make && sudo make install
 
 # Set alias for multitably open
 echo                       >> ~/.bashrc
@@ -28,8 +26,8 @@ echo  "alias vim='vim -p'" >> ~/.fishrc
 
 # Clone my vimrc and create symbolic link
 cd  ~/repositories
-git clone https://github.com/shgt/vimrc.git
-ln  -s ~/repositories/vimrc/.vimrc ~/.vimrc
+git clone https://github.com/matoruru/dotfiles.git
+ln  -s ~/repositories/dotfiles/.vimrc ~/.vimrc
 
 # Install plugin manager for vim, dein
 mkdir -p           ~/.cache/dein
