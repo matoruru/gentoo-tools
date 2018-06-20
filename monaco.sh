233f591e98085ed18a13ebd39f421620384e4c91
matoruru <matoruru@shgt.jp>
2018-06-20 12:41:47 +0000

    add font settings

---
+++ monaco.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/repositories/javivelasco
+cd       ~/repositories/javivelasco
+git clone https://github.com/javivelasco/dotfiles.git
+cd dotfiles/fonts
+sudo cp monaco-linux-powerline.ttf   /usr/share/fonts/
+
+# Edit -> Profile Preferences -> General -> CustomFont
