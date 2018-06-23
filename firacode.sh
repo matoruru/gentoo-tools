8933d5845ac890c41dd9b6f3cd340fd3aa73a42a
matoruru <matoruru@shgt.jp>
2018-06-24 02:15:45 +0900

    delete firacode

--- firacode.sh
+++
@@ -1,8 +0,0 @@
-cd ~/repositories
-git clone https://github.com/tonsky/FiraCode.git
-cd FiraCode/distr/ttf
-sudo cp FiraCode-Light.ttf   /usr/share/fonts/
-sudo cp FiraCode-Regular.ttf /usr/share/fonts/
-sudo cp FiraCode-Retina.ttf  /usr/share/fonts/
-
-# Edit -> Profile Preferences -> General -> CustomFont
