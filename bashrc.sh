af251cac39f4cbc03a5cb8b44c54ed27efd519df
matoruru <matoruru@shgt.jp>
2018-06-24 02:22:45 +0900

    delete files for shell, move it to dotfiles

--- bashrc.sh
+++
@@ -1,2 +0,0 @@
-echo  "alias nano='nano -w'"                  >> ~/.bashrc
-echo  "alias ll='ls -alF'"                    >> ~/.bashrc
--- fish.sh
+++
@@ -1,6 +0,0 @@
-# use it after emergeing fish
-touch    ~/.config/fish/config.fish
-ln    -s ~/.config/fish/config.fish ~/.fishrc
-echo  "alias nano='nano -w'"                  >> ~/.fishrc
-echo  "alias ll='ls -alF'"                    >> ~/.fishrc
-echo  "alias udn='sudo emerge-webrsync;sudo emerge -auDN @world'"       >> ~/.fishrc
