044b43498393ddaed7391caf90c04fa6c2731193
matoruru <matoruru@shgt.jp>
2018-07-02 10:31:51 +0900

    modify indentation due to adding --deep option

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo emerge -av              \
-   dev-lang/ruby             \
-   dev-lang/lua              \
-   dev-lang/luajit           \
-   dev-java/groovy           \
-   dev-vcs/git               \
-   www-client/google-chrome  \
-   sys-kernel/linux-firmware \
-   x11-wm/xmonad             \
-   x11-terms/rxvt-unicode    \
+sudo emerge -avD              \
+   dev-lang/ruby              \
+   dev-lang/lua               \
+   dev-lang/luajit            \
+   dev-java/groovy            \
+   dev-vcs/git                \
+   www-client/google-chrome   \
+   sys-kernel/linux-firmware  \
+   x11-wm/xmonad              \
+   x11-terms/rxvt-unicode     \
    x11-misc/dmenu
 
