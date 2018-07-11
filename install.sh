80f7f7e00620239a571f9c9e7d5a64f0f2f60e10
matoruru <matoruru@shgt.jp>
2018-07-11 21:55:40 +0000

    add sl, xorg

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ sudo emerge -avD              \
    dev-java/groovy            \
    dev-vcs/git                \
    app-shells/fish            \
+   app-misc/sl                \
    www-client/google-chrome   \
    sys-kernel/linux-firmware  \
    media-gfx/feh              \
@@ -24,20 +25,25 @@ sudo emerge -avD              \
    x11-terms/rxvt-unicode     \
    x11-misc/lightdm           \
    x11-misc/dmenu             \
+   x11-base/xorg-server       \
    layman
 
 # check whether necessary files is exist or not
 NECESSARY_FILES=()
-NECESSARY_FILES+=( "/usr/bin/ruby"   )
-NECESSARY_FILES+=( "/usr/bin/lua"    )
-NECESSARY_FILES+=( "/usr/bin/luajit" )
-NECESSARY_FILES+=( "/usr/bin/groovy"   )
-NECESSARY_FILES+=( "/usr/bin/git"   )
-NECESSARY_FILES+=( "/bin/fish"   )
-NECESSARY_FILES+=( "/usr/bin/feh"   )
-NECESSARY_FILES+=( "/usr/bin/xmonad"   )
-NECESSARY_FILES+=( "/usr/bin/urxvt"   )
-NECESSARY_FILES+=( "/usr/bin/dmenu"   )
+NECESSARY_FILES+=( "/usr/bin/ruby"                   )
+NECESSARY_FILES+=( "/usr/bin/lua"                    )
+NECESSARY_FILES+=( "/usr/bin/luajit"                 )
+NECESSARY_FILES+=( "/usr/bin/groovy"                 )
+NECESSARY_FILES+=( "/usr/bin/git"                    )
+NECESSARY_FILES+=( "/bin/fish"                       )
+NECESSARY_FILES+=( "/usr/bin/sl"                     )
+NECESSARY_FILES+=( "/usr/bin/google-chrome-stable"   )
+NECESSARY_FILES+=( "/bin/fish"                       )
+NECESSARY_FILES+=( "/usr/bin/feh"                    )
+NECESSARY_FILES+=( "/usr/bin/xmonad"                 )
+NECESSARY_FILES+=( "/usr/bin/urxvt"                  )
+NECESSARY_FILES+=( "/usr/bin/dmenu"                  )
+NECESSARY_FILES+=( "/usr/bin/layman"                 )
 for file in ${NECESSARY_FILES[@]}
 do
    if [[ ! -f $file ]]; then
