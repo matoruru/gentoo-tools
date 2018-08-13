b7901e9de6cc0fc693dc4b9c9d4efd8016b2ba9f
matoruru <matoruru@shgt.jp>
2018-08-14 00:19:49 +0900

    add xmonad chache remover

---
+++ xmonad-chacheclear.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# when xmonad did not work collectly,
+# execute this script before reboot.
+
+rm ~/.xmonad/xmonad-x86_64-linux
+rm ~/.xmonad/xmonad.hi
+rm ~/.xmonad/xmonad.o
+rm ~/.xmonad/xmonad.state
+rm ~/.xmonad/xmonad.errors
