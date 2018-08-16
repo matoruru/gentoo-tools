9dcbe97e4dc6477a429d1b07f9cf2b368e539221
matoruru <matoruru@shgt.jp>
2018-08-16 14:55:25 +0900

    big modify

--- screenshot.sh
+++ screenshot.sh
@@ -10,7 +10,26 @@ if [[ -f $tmpname ]]; then
    exit
 fi
 
-newname=$( scrot -q 100 $1 '%Y-%m-%d-%T-screenshot.png' -e 'echo $f' )
-cp $newname                  $tmpname
-timeout 1 feh --fullscreen --zoom 98 $tmpname
-rm                           $tmpname
+newname=$( scrot -q 100 $3 '%Y-%m-%d-%T-screenshot.png' -e 'echo $f' )
+cp $newname $tmpname
+
+# To make $disptime from 0 to 5
+if   [[ $(echo "$1 <= 0" | bc) -eq 1 ]]; then
+   echo "redefine $1 as 0.1..."
+   disptime=0.1
+elif [[ $(echo "$1 >  5" | bc) -eq 1 ]]; then
+   echo "redefine $1 as 5..."
+   disptime=5
+else
+   disptime=$1
+fi
+
+zoomratio=$2
+
+timeout $disptime feh --fullscreen --zoom $zoomratio $tmpname
+
+# if $disptime is lower than 1, after sleep 1 - $disptime, rm
+if [[ $(echo "$disptime < 1" | bc) -eq 1 ]]; then
+   sleep $(echo "1 - $disptime" | bc)
+fi
+rm $tmpname
