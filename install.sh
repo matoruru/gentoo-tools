e6ea9229272aef8abcf256edbb88558672627a4c
matoruru <matoruru@shgt.jp>
2019-02-15 13:34:39 +0900

    add ntpd

--- install.sh
+++ install.sh
@@ -145,6 +145,9 @@ sudo ln -srf 40-libinput.conf /usr/share/X11/xorg.conf.d/
 # add dbus to default
 sudo rc-update add dbus default
 
+# use ntpd
+sudo rc-update add ntpd default
+
 # kvm
 sudo gpasswd -a matoruru kvm
 sudo usermod -aG libvirt matoruru
