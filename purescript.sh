cde7094b44422d3e5cc32621490ed4836d61bd43
matoruru <matoruru@shgt.jp>
2019-02-21 01:39:15 +0900

    modify purescript installation

--- purescript.sh
+++ purescript.sh
@@ -2,10 +2,3 @@
 mkdir -p ~/purescript
 cd ~/purescript
 npm i -D purescript pulp
-
-# purs.bin is installed but delete it and compile it
-cd node_modules/.bin
-
-# create links in /usr/local/bin
-sudo ln -sr ./purs /usr/local/bin/
-sudo ln -sr ./pulp /usr/local/bin/
