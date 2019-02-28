b71ba1c857f4db11695a1f3433f592367291a324
matoruru <matoruru@shgt.jp>
2019-02-28 22:02:26 +0900

    modify

--- cursors.sh
+++ cursors.sh
@@ -2,9 +2,6 @@
 
 mkdir ~/.icons
 
-# capitaine-cursors
-ln -s /usr/share/icons/capitaine-cursors ~/.icons/
-
 # breeze-cursors
 sudo tar xvjf breeze-cursors.tar.bz2 -C /usr/share/icons
 ln -s /usr/share/icons/breeze-cursors ~/.icons/
